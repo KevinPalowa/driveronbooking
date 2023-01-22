335d55249276c3d2c1dc4ae4bbc93c71e9f623ce
Kevin Palowa <kevinpalowa@gmail.com>
2023-01-22 19:57:16 +0700

    fix: user api

---
+++ pages/api/user/[id].tsx
@@ -0,0 +1,33 @@
+import prisma from "@/lib/prisma";
+import { NextApiRequest, NextApiResponse } from "next";
+import jwt from "jsonwebtoken";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  const { authorization } = req.headers;
+  const { id } = req.query;
+  // Signed in
+  if (authorization) {
+    const token = jwt.verify(authorization, process.env.JWT_SECRET!);
+    if (token) {
+      switch (req.method) {
+        case "DELETE":
+          const driver = await prisma.user.delete({
+            where: { id: Number(id) },
+          });
+          if (driver) {
+            res.status(200).json({ meta: { message: "Delete success" } });
+          }
+          break;
+      }
+    } else {
+      res.status(400).json({ message: "Invalid JWT" });
+    }
+  } else {
+    res
+      .status(400)
+      .json({ message: "Please input token in authorization header" });
+  }
+}
